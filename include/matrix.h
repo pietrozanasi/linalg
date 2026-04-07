2f84428b80bbbaea993bc043077a9554259d58f8
Pietro Zanasi <pietrozanasi@gmail.com>
2026-04-07 19:23:55 +0200

    added many operations. still needs testing.

--- include/matrix.h
+++ include/matrix.h
@@ -7,6 +7,8 @@ void m_zero_init(matrix* m, int n);
 
 void m_rand_init(matrix* m, int n, double min, double max);
 
+void m_id_init(matrix* m, int n);
+
 double m_at(matrix* m, int i, int j);
 
 void m_set(matrix* m, int i, int j, double x);
@@ -14,3 +16,11 @@ void m_set(matrix* m, int i, int j, double x);
 void m_print(matrix* m);
 
 void m_mult(matrix* m1, matrix* m2, matrix* out);
+
+void m_add(matrix* m1, matrix* m2, matrix* out);
+
+void m_swap_rows(matrix* m, int r1, int r2);
+
+void m_transpose(matrix* m, matrix* out);
+
+void lu_decomp(matrix* m, matrix* L, matrix* U, matrix *P);
--- src/ops.c
+++ src/ops.c
@@ -3,7 +3,7 @@
 #include "../include/matrix.h"
 #include "../include/vector.h"
 
-// initializations
+// initializers
 
 void v_zero_init(vector* v, int n) {
     v->n = n;
@@ -29,6 +29,14 @@ void m_rand_init(matrix* m, int n, double min, double max) {
 
     for(int i = 0; i < n * n; i++) {
         m->data[i] = min + (max - min) * rand() / (double)RAND_MAX;
+    }
+}
+
+void m_id_init(matrix* m, int n) {
+    m->n = n;
+    m->data = (double*)calloc(n * n, sizeof(double));
+    for(int i = 0; i < n; i++) {
+        m_set(m, i, i, 1.0);
     }
 }
 
@@ -69,7 +77,8 @@ void m_set(matrix* m, int i, int j, double x) {
 // vector operations
 
 void v_scalar_mult(vector* v, double c) {
-    for(int i = 0; i < v->n; i++) {
+    int n = v->n;
+    for(int i = 0; i < n; i++) {
         v->data[i] *= c;
     }
 }
@@ -79,7 +88,8 @@ void v_mult(vector* v1, vector* v2, vector* out) {
         fprintf(stderr, "Incompatible vector dimensions\n");
         exit(1);
     }
-    for(int i = 0; i < v1->n; i++) {
+    int n = v1->n;
+    for(int i = 0; i < n; i++) {
         out->data[i] = v1->data[i] * v2->data[i];
     }
 }
@@ -89,7 +99,8 @@ void v_add(vector* v1, vector* v2, vector* out) {
         fprintf(stderr, "Incompatible vector dimensions\n");
         exit(1);
     }
-    for(int i = 0; i < v1->n; i++) {
+    int n = v1->n;
+    for(int i = 0; i < n; i++) {
         out->data[i] = v1->data[i] + v2->data[i];
     }
 }
@@ -102,9 +113,10 @@ void m_mult(matrix* m1, matrix* m2, matrix* out) {
         exit(1);
     }
     int n = m1->n;
+    double sum;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-            double sum = 0.0;
+            sum = 0.0;
             for(int k = 0; k < n; k++) {
                 sum += m_at(m1, i, k) * m_at(m2, k, j);
             }
@@ -113,17 +125,62 @@ void m_mult(matrix* m1, matrix* m2, matrix* out) {
     }
 }
 
-void vm_mult(matrix* m, vector* v, vector* out) {
+void m_add(matrix* m1, matrix* m2, matrix* out) {
+    if(m1->n != m2->n || m1->n != out->n) {
+        fprintf(stderr, "Incompatible matrix dimensions\n");
+        exit(1);
+    }
+    int n = m1->n;
+    for(int i = 0; i < n * n; i++) {
+        out->data[i] = m1->data[i] + m2->data[i];
+    }
+}
+
+// matrix-vector multiplication
+
+void mv_mult(matrix* m, vector* v, vector* out) {
     if(m->n != v->n || m->n != out->n) {
         fprintf(stderr, "Incompatible dimensions for matrix-vector multiplication\n");
         exit(1);
     }
     int n = m->n;
+    double sum;
+
     for(int i = 0; i < n; i++) {
-        double sum = 0.0;
+        sum = 0.0;
         for(int j = 0; j < n; j++) {
             sum += m_at(m, i, j) * v_at(v, j);
         }
         v_set(out, i, sum);
     }
 }
+
+// matrix manipulation
+
+void m_swap_rows(matrix* m, int r1, int r2) {
+    if(r1 >= m->n || r2 >= m->n) {
+        fprintf(stderr, "Row index out of bounds\n");
+        exit(1);
+    }
+    int n = m->n;
+    double temp;
+    for(int j = 0; j < n; j++) {
+        temp = m_at(m, r1, j);
+        m_set(m, r1, j, m_at(m, r2, j));
+        m_set(m, r2, j, temp);
+    }
+}
+
+void m_transpose(matrix* m, matrix* out) {
+    if(m->n != out->n) {
+        fprintf(stderr, "Incompatible dimensions for transpose\n");
+        exit(1);
+    }
+    int n = m->n;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            m_set(out, j, i, m_at(m, i, j));
+        }
+    }
+}
+
